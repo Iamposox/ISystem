6e729321e5fb86737bd7730d29052f1816a094f5
Aleksandr Smirnov <vi_demon@hotmail.de>
2020-03-01 15:01:16 +0100

    changes

---
+++ IS.Domain/Model/TestEntity.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IS.Domain.Model
+{
+    public class TestEntity
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Context : DbContext
+    {
+        public Context()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string ConnectionSting = "Data Source=Test.db";
+            optionsBuilder.UseSqlite(ConnectionSting);
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<TestEntity> Products { get; set; }
+
+    }
+}
